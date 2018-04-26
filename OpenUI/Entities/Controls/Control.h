844b6ec807c43a0226f2e8ec2b6726e9c96b1ded
Beef <double.codes@gmail.com>
2018-04-26 18:17:33 +0200

    dfs

--- OpenUI/Entities/Controls/Control.h
+++ OpenUI/Entities/Controls/Control.h
@@ -1,11 +1,11 @@
 #pragma once
-#include "../Elements/Element.h"
+#include "OpenUI/Entities/Elements/Element.h"
 
 class Control : public Element
 {
 public:
 	explicit Control ( const std::string& name );
-
+	//
 	void Initialize () override;
 	void Update () override;
 	void Draw () override;
